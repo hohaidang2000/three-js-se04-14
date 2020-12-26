6b425ca78ebcd71661fe248b9f0e009ceb786eca
hohaidangpro <72792170+hohaidangpro@users.noreply.github.com>
2020-12-26 21:05:22 +0700

    long time update

--- js/app.js
+++ js/app.js
@@ -1,26 +1,134 @@
+
+
+//commend man
 function init() {
+  function initStats(type) {
+    //var panelType = (typeof type !== 'undefined' && type) && (!isNaN(type)) ? parseInt(type) : 0;
+    var stats = new Stats();
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    
+    
+    document.getElementById("webgl-output").appendChild(stats.dom);
+
+    return stats;
+    }
+    var stats = initStats();
+
+    // default setup
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
 
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    renderer.setClearColor(new THREE.Color(0x000000));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    
+    // create the ground plane
+    var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
+    var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
 
-    camera.position.z = 5;
+    // rotate and position the plane
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
 
-    var animate = function () {
-        requestAnimationFrame( animate );
+    // add the plane to the scene
+    scene.add(plane);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+    // create a cube
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.castShadow = true;
 
-        renderer.render( scene, camera );
-    };
+    // position the cube
+    cube.position.x = -4;
+    cube.position.y = 4;
+    cube.position.z = 0;
 
-    animate();
+    // add the cube to the scene
+    scene.add(cube);
+
+    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+    // position the sphere
+    sphere.position.x = 20;
+    sphere.position.y = 0;
+    sphere.position.z = 2;
+    sphere.castShadow = true;
+
+    // add the sphere to the scene
+    scene.add(sphere);
+
+    // position and point the camera to the center of the scene
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+
+    // add subtle ambient lighting
+    var ambienLight = new THREE.AmbientLight(0x353535);
+    scene.add(ambienLight);
+
+    // add spotlight for the shadows
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-10, 20, -5);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+
+    // add the output of the renderer to the html element
+    document.getElementById("webgl-output").appendChild(renderer.domElement);
+    
+    //var effect = new THREE.AsciiEffect( renderer );
+    //effect.setSize( window.innerWidth, window.innerHeight );
+    //document.getElementById("webgl-output").appendChild(effect.domElement);
+    
+
+    var controls = new function() {
+      this.rotationSpeed = 0.02;
+      this.bouncingSpeed = 0.03;
+      }
+    var gui = new dat.GUI();
+    gui.add(controls, 'rotationSpeed',0,0.5);
+    gui.add(controls, 'bouncingSpeed',0,0.5);  
+    // call the render function
+
+
+
+    var step = 0;
+    renderScene();
+    
+    function renderScene() {
+        
+        stats.update();
+        
+        // rotate the cube around its axes
+        cube.rotation.x += controls.rotationSpeed;;
+        cube.rotation.y += controls.rotationSpeed;;
+        cube.rotation.z += controls.rotationSpeed;;
+
+        // bounce the sphere up and down
+        step += controls.bouncingSpeed;
+        sphere.position.x = 20 + (10 * (Math.cos(step)));
+        sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+        // render using requestAnimationFrame
+        renderer.render(scene, camera);
+        requestAnimationFrame(renderScene);
+        
+        //effect.render(scene, camera)
+    
+  }
+
+  
 }
+
+
